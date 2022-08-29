778621add768f7e3a2c5b890fec8b6b530cae1b0
Jean Julio <jean.ferreira@soulassale.com.br>
2022-08-29 15:18:10 -0300

    Relacionamento Muitos Pra Muitos

---
+++ POO/composicao/CursoTeste.java
@@ -0,0 +1,40 @@
+package POO.composicao;
+
+public class CursoTeste {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno("Joao");
+		Aluno aluno2 = new Aluno("Maria");
+		Aluno aluno3 = new Aluno("Pedro");
+		
+		Curso curso1 = new Curso("Java Completo");
+		Curso curso2 = new Curso("Web Completo");
+		Curso curso3 = new Curso("React Native");
+		
+		curso1.adicionarAluno(aluno1);
+		curso1.adicionarAluno(aluno2);
+		
+		curso2.adicionarAluno(aluno1);
+		curso2.adicionarAluno(aluno2);
+		
+		aluno1.adicionarCurso(curso3);
+		aluno2.adicionarCurso(curso3);
+		aluno3.adicionarCurso(curso3);
+		
+		for (Aluno aluno: curso3.alunos) {
+			System.out.println("Estou matriculado no curso " + curso3.nome + "....");
+			System.out.println("E o meu nome é " + aluno.nome);
+		}
+		
+		System.out.println(aluno1.cursos.get(0).alunos);
+		
+		Curso cursoEncontrado = aluno1.obterCursoPorNome("Java Completo");
+		if(cursoEncontrado != null) {
+			System.out.println(cursoEncontrado.nome);
+			System.out.println(cursoEncontrado.alunos);
+		}
+		
+	}
+
+}

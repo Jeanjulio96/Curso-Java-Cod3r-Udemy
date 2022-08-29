22c3ec8b0ba9fa28450fea310e538c52e6ec1a20
Jean Julio <jean.ferreira@soulassale.com.br>
2022-08-29 13:56:27 -0300

    Relacionamento Um Pra Um

---
+++ POO/composicao/CarroTeste.java
@@ -0,0 +1,34 @@
+package POO.composicao;
+
+public class CarroTeste {
+	
+	public static void main(String[] args) {
+		
+		Carro c1 = new Carro();
+		
+		c1.ligar();
+		
+		System.out.println(c1.estaLigado());
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		System.out.println(c1.motor.giros());
+		
+		c1.frear();
+		c1.frear();
+		c1.frear();
+		c1.frear();
+		
+		// Relação Bidirecional
+		System.out.println(c1.motor.carro.motor.carro.motor.giros());
+		
+		
+		/* Faltou Encapsulamento!!!
+		c1.motor.fatorInjecao = -30; */
+	}
+
+}
